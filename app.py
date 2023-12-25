65f6a0f56c5c05eebe8651b5560587cf8653b1de
hrisabhy <hrisabhyadav31@gmail.com>
2023-12-25 15:52:03 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -18,3 +18,69 @@ df = pdr.get_data_yahoo(user_input, start, end)
 # Describing Data
 st.subheader('Data from 2010-2019')
 st.write(df.describe())
+
+#Visualization
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize=(12, 6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize=(12, 6))
+plt.plot(ma100)
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize=(12, 6))
+plt.plot(ma100, 'r')
+plt.plot(ma200, 'g')
+plt.plot(df.Close, 'b')
+st.pyplot(fig)
+
+# Splitting the data into training and testing dataset
+data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.7)])
+data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.7) : int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+data_training_array= scaler.fit_transform(data_training)
+
+# Load model
+model = load_model('./Model/keras_model.h5')
+
+# Testing part
+past_100_days = data_training.tail(100)
+final_df = past_100_days.append(data_testing, ignore_index = True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i - 100: i])
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+
+y_pred = model.predict(x_test)
+
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_pred = y_pred * scale_factor
+y_test = y_test * scale_factor
+
+# Final Graph
+st.subheader('Predictions vs Original')
+fig2 = plt.figure(figsize=(12, 6))
+plt.plot(y_test, 'b', label = 'Original Price')
+plt.plot(y_pred, 'r', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
